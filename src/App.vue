89b69353b8f4fdc7d6979567bcd6cb47e624f73d
danielaleksandrov <crunchyhotline@gmail.com>
2021-04-01 17:09:53 -0700

    'new-blueberry-project'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/recipes">Recipes</router-link> |
+    <router-link to="/freezing">Freezing</router-link>
   </div>
   <router-view />
 </template>
