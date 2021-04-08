ef678660931680514fbc03d903983843830cc95b
danielaleksandrov <crunchyhotline@gmail.com>
2021-04-07 19:52:11 -0700

    'qs'

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
 <template>
   <div style="position: relative; height: 0px">
     <i @click="navModal = !navModal" class="fas fa-bars fa-4x sandwich-nav"></i>
-    <div v-if="navModal" class="mobile-nav">
-      <router-link @click="navModal = false" class="mobile-nav-item" to="/"
-        >Home</router-link
-      >|
-      <router-link
-        @click="navModal = false"
-        class="mobile-nav-item"
-        to="/recipes"
-        >Recipes</router-link
-      >|
-      <router-link
-        @click="navModal = false"
-        class="mobile-nav-item"
-        to="/freezing"
-        >Freezing</router-link
+    <div @click="navModal = !navModal" v-if="navModal" class="mobile-nav">
+      <router-link class="nav-item" to="/"> Home</router-link> |
+      <router-link class="nav-item" to="/recipes"> Recipes</router-link> |
+      <router-link class="nav-item" to="/freezing"> Freezing</router-link>
       >
     </div>
   </div>
